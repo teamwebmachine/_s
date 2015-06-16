e5f29ce2363c87d24663332d69309ce8bdb6b611
Christopher Mosure <cj@webmachine.io>
2015-06-16 09:43:33 -0400

    Housekeeping

--- search.php
+++ search.php
@@ -7,7 +7,7 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
+	<section id="primary" class="container-fluid content-area">
 		<main id="main" class="site-main" role="main">
 
 		<?php if ( have_posts() ) : ?>
